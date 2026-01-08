79c3e797ecbb57b864b5aba2a1b8aaf6c3a117f4
Chhavi <chhavichhokar.23@gmail.com>
2026-01-09 00:28:18 +0530

    Load Firebase Admin credentials from env variables

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,19 +1,17 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const serviceAccountPath = path.join(
-  __dirname,
-  "serviceAccountKey.json"
-);
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(serviceAccountPath, "utf-8")
-);
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
